69a01e983a6cb6faee2c2047f9059f1356ef9b24
annacasavant <anca7065@colorado.edu>
2024-11-04 09:07:13 -0700

    Renamed to SiennaTemplate, added structure for Reference/Tutorials/How-to, moved public/internal under Reference

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,27 @@
-using Documenter, SIIP - PACKAGE
+using Documenter
+import DataStructures: OrderedDict
+using SiennaTemplate
 
 pages = OrderedDict(
     "Welcome Page" => "index.md",
-    "Quick Start Guide" => "quick_start_guide.md",
-    "Tutorials" => "tutorials/intro_page.md",
-    "Public API Reference" => "api/public.md",
-    "Internal API Reference" => "api/internal.md",
+    "Tutorials" =>  Any["stub" => "tutorials/stub.md"],
+    "How to..." =>  Any["stub" => "how_to_guides/stub.md"],
+    "Explanation" => Any["stub" => "explanation/stub.md"],
+    "Reference" =>
+            Any["Public API" => "reference/public.md",
+            "Internals" => "reference/internal.md"]
 )
 
 makedocs(
-    modules=[SIIP-PACKAGE],
+    modules=[SiennaTemplate],
     format=Documenter.HTML(prettyurls=haskey(ENV, "GITHUB_ACTIONS")),
-    sitename="SIIP-PACKAGE.jl",
+    sitename="github.com/NREL-Sienna/SiennaTemplate.jl",
     authors="Freddy Mercury, Nikola Tesla, Leonard Bernestein",
     pages=Any[p for p in pages],
 )
 
 deploydocs(
-    repo="github.com/NREL-SIIP/SIIP-PACKAGE.git",
+    repo="github.com/NREL-Sienna/SiennaTempalte.jl",
     target="build",
     branch="gh-pages",
     devbranch="master",
@@ -25,3 +29,5 @@ deploydocs(
     push_preview=true,
     versions=["stable" => "v^", "v#.#"],
 )
+
+
