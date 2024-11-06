ce3c10489e53c5c502224781b64b408fe92a9b3c
annacasavant <anca7065@colorado.edu>
2024-11-06 13:11:45 -0700

    edited make.jl for new name/diataxis format

--- docs/make.jl
+++ docs/make.jl
@@ -4,24 +4,28 @@ using SiennaTemplate
 
 pages = OrderedDict(
     "Welcome Page" => "index.md",
-    "Tutorials" =>  Any["stub" => "tutorials/stub.md"],
-    "How to..." =>  Any["stub" => "how_to_guides/stub.md"],
+    "Tutorials" => Any["stub" => "tutorials/stub.md"],
+    "How to..." => Any["stub" => "how_to_guides/stub.md"],
     "Explanation" => Any["stub" => "explanation/stub.md"],
-    "Reference" =>
-            Any["Public API" => "reference/public.md",
-            "Internals" => "reference/internal.md"]
+    "Reference" => Any[
+        "Public API" => "reference/public.md",
+        "Internals" => "reference/internal.md",
+    ],
 )
 
 makedocs(
-    modules=[SiennaTemplate],
-    format=Documenter.HTML(prettyurls=haskey(ENV, "GITHUB_ACTIONS")),
-    sitename="github.com/NREL-Sienna/SiennaTemplate.jl",
-    authors="Freddy Mercury, Nikola Tesla, Leonard Bernestein",
-    pages=Any[p for p in pages],
+    modules = [SiennaTemplate],
+    format = Documenter.HTML(
+        prettyurls = haskey(ENV, "GITHUB_ACTIONS"),
+        size_threshold = nothing,),
+    sitename = "github.com/NREL-Sienna/SiennaTemplate.jl",
+    authors = "Freddy Mercury",
+    pages = Any[p for p in pages],
+    draft = false,
 )
 
 deploydocs(
-    repo="github.com/NREL-Sienna/SiennaTempalte.jl",
+    repo="github.com/annacasavant/SiennaTempalte.jl",
     target="build",
     branch="gh-pages",
     devbranch="master",
@@ -29,5 +33,4 @@ deploydocs(
     push_preview=true,
     versions=["stable" => "v^", "v#.#"],
 )
-
 
